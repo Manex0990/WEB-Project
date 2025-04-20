e2398602bf470f7f227ccd0328cfe6fa6b675733
Admin <arsenijcugajnov709@gmail.com>
2025-04-20 12:53:13 +0300

    ПОЧТИ working "square" task

--- main.py
+++ main.py
@@ -19,21 +19,40 @@ funcs = {'Квадратное уравнение': [ex.generate_square_x, ex.ch
          'Пример на деление': [ex.generate_crop_stage_1, ex.check_answer_for_all_stages]}
 
 
-# добавить наследование html
 @app.route('/task/<title>', methods=['GET', 'POST'])
 def open_task(title):
     form = TaskForm()
     task = funcs[names[title]][0]()
     title_html = names[title]
-    if form.validate_on_submit():
+    solution_generation = {'Квадратное уравнение': ['Сначала найдем дискриминант квадратного уравнения:',
+                                                    'Если дискриманант больше нуля, то будет 2 корня',
+                                                    'Если равен нулю, то будет 1 корень',
+                                                    'Если меньше нуля, то Корней нет.',
+                                                    f'D = b\u00B2 - 4ac; D = {ex.find_discriminant(task)}',
+                                                    'Теперь можно найти корни(корень) уравнения',
+                                                    'x1 = (-b - \u221AD) / 2a',
+                                                    'x2 = (-b + \u221AD) / 2a',
+                                                    f'Ответ: {ex.answer_square_x(task)}'],
+                           'Линейное уравнение': ['Решим линейное уравнение'],
+                           'Пример на сложение': ['Просто сложим все коэффициенты',
+                                                  f'Ответ: {ex.answer_for_all_stages(task)}'],
+                           'Пример на вычитание': ['Просто вычтем все коэффициенты',
+                                                   f'Ответ: {ex.answer_for_all_stages(task)}'],
+                           'Пример на умножение': ['Просто перемножим все коэффициенты',
+                                                   f'Ответ: {ex.answer_for_all_stages(task)}'],
+                           'Пример на деление': ['Просто разделим по порядку все коэффициенты',
+                                                 f'Ответ: {ex.answer_for_all_stages(task)}']}
+    if request.method == 'POST':
         user_answer = form.answer.data
-        if ex.check_answer_square_x(task, user_answer):
-            return render_template('task_opened.html', title=title_html,
-                                   form=form, message='Верно')
+        verdict = ex.check_answer_square_x(task, user_answer)
+        if verdict[1]:
+            return render_template('solution.html', title=title_html, task=task, form=form,
+                                   solution_log=solution_generation[title_html], message=verdict[0])
         else:
-            return render_template('task_opened.html', title=title_html,
-                                   form=form, message='Неверно или неверный формат ответа')
-    return render_template('solution.html', title=title_html, task=task, form=form)
+            return render_template('solution.html', title=title_html, task=task, form=form,
+                                   solution_log=solution_generation[title_html],
+                                   message=verdict[0])
+    return render_template('task_opened.html', title=title_html, task=task, form=form)
 
 
 # нужно разделить обработчики для решения квадратного, линейного, примеров.
