eb3b4c8a2fe996d9011da5a474445314289dfc5b
Admin <arsenijcugajnov709@gmail.com>
2025-04-26 18:33:46 +0300

    all working cookie on all tasks (finally) ;)

--- main.py
+++ main.py
@@ -34,20 +34,16 @@ funcs = {'Квадратное уравнение': [ex.generate_square_x, ex.ch
          'Пример на деление (простой)': [ex.generate_crop_stage_1, ex.check_answer_for_all_stages],
          'Пример на деление (средний)': [ex.generate_crop_stage_2, ex.check_answer_for_all_stages],
          'Пример на деление (сложный)': [ex.generate_crop_stage_3, ex.check_answer_for_all_stages]}
-c_line, c_ex = 0, 0
-temp_line, temp_ex = '', ''  # поменять на cookie
 
 
 @app.route('/task/square', methods=['GET', 'POST'])
 def open_task_square():
-    visits_count = int(request.cookies.get("visits_count", 0))
-    task = str(request.cookies.get('cur_task', funcs[names['square']][0]()))
+    task = str(request.cookies.get('cur_task_square', funcs[names['square']][0]()))
     form = TaskForm()
     title_html = names['square']
     page = make_response(render_template('task_opened.html', title=title_html,
                                          task=task, form=form))
-    page.set_cookie('visits_count', str(visits_count + 1), max_age=60 * 60 * 24 * 365 * 2)
-    page.set_cookie('cur_task', value=str(task), max_age=60 * 60 * 24 * 365 * 2)
+    page.set_cookie('cur_task_square', value=str(task), max_age=60 * 60 * 24 * 365 * 2)
     solution_generation = ['Сначала найдем дискриминант квадратного уравнения:',
                            'Если дискриманант больше нуля, то будет 2 корня',
                            'Если равен нулю, то будет 1 корень',
@@ -64,122 +60,82 @@ def open_task_square():
             res = make_response(
                 render_template('solution.html', title=title_html, task=task,
                                 form=form, solution_log=solution_generation, message=verdict[0]))
-            res.set_cookie("visits_count", '0', max_age=60 * 60 * 24 * 365 * 2)
-            res.set_cookie('cur_task', '', max_age=0)
+            res.set_cookie('cur_task_square', '', max_age=0)
         else:
             res = make_response(
                 render_template('solution.html', title=title_html, task=task,
                                 form=form, solution_log=['Дайте верный ответ, чтобы получить решение.'],
                                 message=verdict[0]))
-            res.set_cookie("visits_count", str(visits_count + 1), max_age=60 * 60 * 24 * 365 * 2)
         return res
     return page
 
 
 @app.route('/task/line', methods=['GET', 'POST'])
 def open_task_line():
-    global c_line
-    global temp_line
-    title = 'line'
+    task = str(request.cookies.get('cur_task_line', funcs[names['line']][0]()))
     form = TaskForm()
-    if c_line == 0:
-        task = funcs[names[title]][0]()
-        temp_line = task
-    title_html = names[title]
+    title_html = names['line']
+    page = make_response(render_template('task_opened.html', title=title_html,
+                                         task=task, form=form))
+    page.set_cookie('cur_task_line', value=str(task), max_age=60 * 60 * 24 * 365 * 2)
     solution_generation = ['Для того, чтобы решить линейное уравнение нужно все коэффициенты с "х"',
                            'перенести в одну часть уравнения, а остальные в другую.',
-                           f'Ответ: {ex.answer_line_x(temp_line)}']
+                           f'Ответ: {ex.answer_line_x(task)}']
     if request.method == 'POST':
         user_answer = form.answer.data
-        verdict = funcs[names[title]][1](temp_line, user_answer)
+        verdict = funcs[names['line']][1](task, user_answer)
         if verdict[1]:
-            c_line += 1
-            return render_template('solution.html', title=title_html, task=temp_line, form=form,
-                                   solution_log=solution_generation, message=verdict[0])
+            res = make_response(
+                render_template('solution.html', title=title_html, task=task,
+                                form=form, solution_log=solution_generation, message=verdict[0]))
+            res.set_cookie('cur_task_line', '', max_age=0)
         else:
-            c_line += 1
-            return render_template('solution.html', title=title_html, task=temp_line, form=form,
-                                   solution_log=['Дайте верный ответ, чтоб получить решение'],
-                                   message=verdict[0])
-    c_line += 1
-    return render_template('task_opened.html', title=title_html, task=temp_line, form=form)
+            res = make_response(
+                render_template('solution.html', title=title_html, task=task,
+                                form=form, solution_log=['Дайте верный ответ, чтобы получить решение.'],
+                                message=verdict[0]))
+        return res
+    return page
 
 
 @app.route('/task/<title>/<level>', methods=['GET', 'POST'])
 def open_task_examples_all_stages(title, level):
-    global c_ex
-    global temp_ex
-    form = TaskForm()
     full_name = '_'.join([title, level])
-    if c_ex == 0:
-        task = funcs[names[full_name]][0]()
-        temp_ex = task
+    task = str(request.cookies.get('cur_task_ex', funcs[names[full_name]][0]()))
+    form = TaskForm()
     title_html = names[full_name]
+    page = make_response(render_template('task_opened.html', title=title_html,
+                                         task=task, form=form))
+    page.set_cookie('cur_task_ex', value=str(task), max_age=60 * 60 * 24 * 365 * 2)
     solution_generation = {'Пример на сложение': ['Просто сложим все коэффициенты',
-                                                  f'Ответ: {ex.answer_for_all_stages(temp_ex)}'],
+                                                  f'Ответ: {ex.answer_for_all_stages(task)}'],
                            'Пример на вычитание': ['Просто вычтем все коэффициенты',
-                                                   f'Ответ: {ex.answer_for_all_stages(temp_ex)}'],
+                                                   f'Ответ: {ex.answer_for_all_stages(task)}'],
                            'Пример на умножение': ['Просто перемножим все коэффициенты',
-                                                   f'Ответ: {ex.answer_for_all_stages(temp_ex)}'],
+                                                   f'Ответ: {ex.answer_for_all_stages(task)}'],
                            'Пример на деление': ['Просто разделим по порядку все коэффициенты',
-                                                 f'Ответ: {ex.answer_for_all_stages(temp_ex)}']}
+                                                 f'Ответ: {ex.answer_for_all_stages(task)}']}
     if request.method == 'POST':
         user_answer = form.answer.data
-        verdict = funcs[names[full_name]][1](temp_ex, user_answer)
+        verdict = funcs[names[full_name]][1](task, user_answer)
         if verdict[1]:
-            c_ex += 1
-            return render_template('solution.html', title=title_html, task=temp_ex, form=form,
-                                   solution_log=solution_generation[title_html[:-10]], message=verdict[0])
+            res = make_response(
+                render_template('solution.html', title=title_html, task=task, form=form,
+                                solution_log=solution_generation[title_html[:-10]], message=verdict[0]))
+            res.set_cookie('cur_task_ex', '', max_age=0)
         else:
-            c_ex += 1
-            return render_template('solution.html', title=title_html, task=temp_ex, form=form,
-                                   solution_log=['Дайте верный ответ, чтоб получить решение'],
-                                   message=verdict[0])
-    c_ex += 1
-    return render_template('task_opened.html', title=title_html, task=temp_ex, form=form)
+            res = make_response(
+                render_template('solution.html', title=title_html, task=task,
+                                form=form, solution_log=['Дайте верный ответ, чтобы получить решение.'],
+                                message=verdict[0]))
+        return res
+    return page
 
 
-# придумать как сократить копипасту в функции open_task_square и open_task_line (после строки if request.method == 'POST') !!!!!!!!
 # возможен баг с проверкой примеров из-за (не 5 а 5.0)
-def solution():
-    pass
-
-
 @app.route('/task')
 def open_task_menu():
-    global c_sq, c_line, c_ex
-    global temp_sq, temp_line, temp_ex
-    c_sq, c_line, c_ex = 0, 0, 0
-    temp_sq, temp_line, temp_ex = '', '', ''
     return render_template('task_window.html')
-
-
-# нужно разделить обработчики для решения квадратного, линейного, примеров.
-# засунуть все в функцию open_task
-# @app.route('/task/<title>/solution')
-# def open_solution(title):
-#    task = funcs[names[title]][0]()
-#    title_html = names[title]
-#    solution_generation = {'Квадратное уравнение': ['Сначала найдем дискриминант квадратного уравнения:',
-#                                                    'Если дискриманант больше нуля, то будет 2 корня',
-#                                                    'Если равен нулю, то будет 1 корень',
-#                                                    'Если меньше нуля, то Корней нет.',
-#                                                    f'D = b\u00B2 - 4ac; D = {ex.find_discriminant(task)}',
-#                                                    'Теперь можно найти корни(корень) уравнения',
-#                                                    'x1 = (-b - \u221AD) / 2a',
-#                                                    'x2 = (-b + \u221AD) / 2a',
-#                                                    f'Ответ: {ex.answer_square_x(task)}'],
-#                           'Линейное уравнение': ['Решим линейное уравнение'],
-#                           'Пример на сложение': ['Просто сложим все коэффициенты',
-#                                                  f'Ответ: {ex.answer_for_all_stages(task)}'],
-#                           'Пример на вычитание': ['Просто вычтем все коэффициенты',
-#                                                   f'Ответ: {ex.answer_for_all_stages(task)}'],
-#                           'Пример на умножение': ['Просто перемножим все коэффициенты',
-#                                                   f'Ответ: {ex.answer_for_all_stages(task)}'],
-#                           'Пример на деление': ['Просто разделим по порядку все коэффициенты',
-#                                                 f'Ответ: {ex.answer_for_all_stages(task)}']}
-#    return render_template('solution.html', title=title_html, task=task,
-#                           solution_log=solution_generation[title_html])
 
 
 if __name__ == '__main__':
