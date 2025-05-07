cb07f684649386cd9d9fe609c7f2edbeada58b07
Admin <arsenijcugajnov709@gmail.com>
2025-05-07 23:35:03 +1000

    first merge releaze

--- task.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField
-from wtforms import SubmitField
-
-
-class TaskForm(FlaskForm):
-    answer = StringField('Ответ')
-    submit = SubmitField('Ответить')
